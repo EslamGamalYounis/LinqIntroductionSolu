59fc020d4ecdc2ea43e0c16f0e68da1925330d05
Islam Gamal <eslam.gamal.younis@gmail.com>
2021-09-13 00:34:50 +0200

    Add project files.

---
+++ Features/MyLinq.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+
+namespace Features
+{
+    public static class MyLinq
+    { 
+        public static int Count(this IEnumerable sequence)
+        {
+            int count = 0;
+            foreach(var item in sequence)
+            {
+                count += 1;
+            }
+            return count;
+        }
+    }
+}
---
+++ Features/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Features;
+
+namespace Features
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<Employee> developers = new Employee[]
+            {
+                new Employee {Id=1, Name="Scoot"},
+                new Employee {Id=2,Name="Alex"}
+            };
+
+            IEnumerable<Employee> sales = new List<Employee>()
+            {
+                new Employee { Id= 3, Name ="Alex"}
+            };
+
+            //foreach (var person in sales)
+            //{
+            //    Console.WriteLine(person.Name);
+            //}
+
+            Console.WriteLine(developers.Count());
+
+            IEnumerator<Employee> enumerator = developers.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current.Name);
+            }   
+
+        }
+    }
+}
